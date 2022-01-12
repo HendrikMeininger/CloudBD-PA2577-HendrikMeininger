a1fe72c875ed960afe9878729a1a0da26645f706
HendrikMeininger <Hendrik.Meininger@stud-mail.uni-wuerzburg.de>
2022-01-12 21:37:16 +0100

    Added first three assignments
    Added Provisioning
    Added MultiMachineSetup
    Added EnterTheCloud

---
+++ Assignments/Enter the Cloud/MultiMachine/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["json",[">= 0"]],["ruby2_keywords",[">= 0.0.4"]],["faraday-net_http",["~> 2.0"]],["faraday",[">= 0.8.6"]],["vagrant-digitalocean",["= 0.9.5"]],["vagrant-puppet-install",["= 7.0.0"]]],"checksum":"c077151085a11144205e809d54f08dab50601e191b33f8b36b25b293dbe0957f","vagrant_version":"2.2.18"}
---
+++ Assignments/Enter the Cloud/MultiMachine/manifests/default.pp
@@ -0,0 +1,33 @@
+node /^appserver/ {
+  exec { 'update':
+    path => ['/usr/bin', '/usr/sbin', '/bin'],
+    command => "apt-get update",
+  }
+
+  package { curl:
+    ensure => "installed",
+    require => Exec['update'],
+  }
+
+  exec {'before installing node.js':
+    path => ['/usr/bin', '/usr/sbin', '/bin'],
+    command => "curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -",
+  }
+
+  package { nodejs:
+    ensure => "installed",
+    require => [ 
+    Exec['update'],
+	Exec['before installing node.js']
+    ],
+  }
+}
+
+node /^dbserver/ {
+  package { 'mysql-server':
+    ensure => latest,
+  }
+}
+
+node default {
+}
